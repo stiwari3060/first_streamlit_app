3cca7242e27556c2361a7dfeadccb0b2a7a908c7
stiwari3060 <160128702+stiwari3060@users.noreply.github.com>
2024-02-15 07:05:52 -0800

    Update streamlit_app.py
    removed error

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
-streamlit.title('My Moms's New Healthy Diner')
+streamlit.title('My "Moms's" New Healthy Diner')
 streamlit.header('Breakfast Favourite Menu')
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('🐔Changes are appearing')
